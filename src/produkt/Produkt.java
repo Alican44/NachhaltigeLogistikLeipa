15d966c4650d2391fe133df97d4c05996fb09793
alican <alican.yilmaz1@hs-augsburg.de>
2023-04-24 17:35:40 +0200

    initial commit

---
+++ src/produkt/Produkt.java
@@ -0,0 +1,10 @@
+package produkt;
+public class Produkt {
+    static public int id;
+    static public double gewicht;
+    static public String standort;
+    static public double lat;           // Breitengrad auf der Weltkarte
+    static public double lon;           // Laengengrad auf der Weltkarte
+}
+
+
---
+++ src/produkt/PullPack.java
@@ -0,0 +1,12 @@
+package produkt;
+
+public class PullPack extends Produkt {
+    public PullPack(){
+        this.id = 1;
+        this.gewicht = 6;
+        this.standort = "Schwedt";
+        this.lat = 53.0625;
+        this.lon = 14.2735;
+    }
+
+}
